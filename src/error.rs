7b50aec0a7ea68cb443baa6e496c0731dadcb863
Andres <andrsbtrg@gmail.com>
2023-08-30 16:25:17 +0200

    Add client side errors and log

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,7 @@ use axum::http::StatusCode;
 pub type Result<T> = core::result::Result<T, Error>;
 
 /// An error Enum for the server
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, strum_macros::AsRefStr)]
 pub enum Error {
     /// Login failed
     LoginFail,
@@ -25,6 +25,43 @@ impl IntoResponse for Error {
     fn into_response(self) -> askama_axum::Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder Axum response
+
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        response.extensions_mut().insert(self);
+
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            // Auth errors
+            Self::AuthFailNoAuthToken
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // Model errors
+            Self::TicketNotFound { .. } => (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS),
+
+            // fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -4,12 +4,12 @@ pub mod mw_auth;
 pub mod routes_static;
 pub mod tickets;
 
-use askama_axum::Response;
-use axum::{middleware, Extension, Router};
+use askama_axum::{IntoResponse, Response};
+use axum::{middleware, Extension, Json, Router};
 use sqlx::SqlitePool;
 use tower_cookies::CookieManagerLayer;
 
-use crate::models::ModelController;
+use crate::{models::ModelController, Error};
 
 use self::mw_auth::mw_async_resolver;
 
@@ -36,6 +36,30 @@ pub fn app(pool: SqlitePool) -> Router {
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "MAPPER");
 
+    let uuid = uuid::Uuid::new_v4();
+
+    let service_error = res.extensions().get::<Error>();
+
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // If client error, build new response
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = serde_json::json!({
+                "error": {
+                "type": client_error.as_ref(),
+                "req_uuid": uuid.to_string(),
+            }
+            });
+            println!("->> client_error_body: {client_error_body}");
+            // Build the new response
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    println!("->> server log line - {uuid} - Error: {service_error:?}");
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
