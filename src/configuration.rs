57afdce8a1ce2ce70d3e4e5c64a960ec1d5c01ec
Andres <andrsbtrg@gmail.com>
2023-10-28 14:47:50 +0200

    Refactor config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,13 +1,18 @@
 use config::{Config, File};
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(serde::Deserialize)]
 pub struct Settings {
-    pub application_port: u16,
-    pub database_name: String,
+    pub addr: String,
+    pub port: u16,
+    pub database: DatabaseConfig,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseConfig {
     pub username: String,
     pub password: String,
     pub host: String,
-    pub application_addr: String,
+    pub db_name: String,
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
@@ -21,22 +26,10 @@ impl Settings {
     pub fn postgres_connection_string(&self) -> String {
         format!(
             "postgres://{}:{}@{}/{}",
-            self.username, self.password, self.host, self.database_name
+            self.database.username,
+            self.database.password,
+            self.database.host,
+            self.database.db_name
         )
-    }
-
-    pub async fn db_check(&self) {
-        let _db_url = self.postgres_connection_string();
-        println!("implement a measure to test if database is up");
-
-        // if !Sqlite::database_exists(&db_url).await.unwrap_or(false) {
-        //     println!("->> {:<12} - CREATING DATABASE {}", "STARTUP", &db_url);
-        //     match Sqlite::create_database(&db_url).await {
-        //         Ok(_) => println!("->> {:<12} - DATABASE CREATED", "STARTUP"),
-        //         Err(error) => panic!("error: {}", error),
-        //     }
-        // } else {
-        //     println!("->> {:<12} - DATABASE EXISTS {}", "STARTUP", &db_url);
-        // }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,14 @@ use std::net::{IpAddr, SocketAddr};
 async fn main() {
     let config = configuration::get_configuration().expect("Unable to load configurationfile.");
 
-    let ip: IpAddr = config.application_addr.parse().unwrap();
-    let socket = SocketAddr::new(ip, config.application_port);
+    let ip: IpAddr = config.addr.parse().unwrap();
+    let socket = SocketAddr::new(ip, config.port);
 
     let db_url = config.postgres_connection_string();
-    config.db_check().await;
 
     let connection_pool = PgPool::connect(&db_url)
         .await
         .expect("Failed to connect to Postgres.");
-    // let db = SqlitePool::connect(&db_url).await.unwrap();
 
     let app = web::app(connection_pool);
     println!("->> {:<12} - LISTENING on http://{}", "RUNNING", socket);
